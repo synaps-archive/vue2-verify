fe1c2e269fb53cc3cb8edd26ae3d8a4cc6c0db6d
Omar Sy <syomar.pro@gmail.com>
2022-05-03 13:22:07 +0200

    first commit

---
+++ example/src/main.js
@@ -0,0 +1,7 @@
+import Vue from 'vue'
+import App from './App.vue'
+Vue.config.productionTip = false
+console.log('hello')
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
---
+++ src/components/SynapsVerify.vue
@@ -0,0 +1,82 @@
+<template>
+  <iframe
+    :src="url"
+    :style="{
+      'min-width': '400px',
+      'min-height': '687px',
+    }"
+    class="root"
+    allow="microphone; camera; midi; encrypted-media;"
+    allowfullscreen="true"
+    frameBorder="none"
+  />
+</template>
+
+<script>
+const serviceUrl = {
+  individual: "https://verify.synaps.io",
+  corporate: "https://verify-v3.synaps.io",
+};
+export default {
+  name: "SynapsVerify",
+  props: {
+    sessionId: {
+      type: String,
+    },
+    service: {
+      type: String,
+    },
+    onReady: {
+      type: Function,
+    },
+    onFinish: {
+      type: Function,
+    },
+    color: {
+      type: Object,
+    },
+    lang: {
+      type: String,
+    },
+    tier: {
+      type: Number,
+    },
+  },
+  computed: {
+    url() {
+      const { sessionId, service, color, lang, tier } = this;
+      const params = {
+        service,
+        session_id: sessionId,
+        primary_color: color ? color.primary : undefined,
+        secondary: color ? color.secondary : undefined,
+        lang: lang ? lang : "en",
+        tier: tier ? tier : undefined,
+      };
+      return `${serviceUrl[service]}?${Object.keys(params)
+        .reduce((acc, key) => {
+          if (params[key]) {
+            acc.push(`${key}=${params[key]}`);
+          }
+          return acc;
+        }, [])
+        .join("&")}`;
+    },
+  },
+  methods: {
+    _listernerMessage({ data }) {
+      if (data.type === "ready") {
+        this.$emit(data.type);
+      } else if (data.type === "finish") {
+        this.$emit(data.type);
+      }
+    },
+  },
+  mounted() {
+    window.addEventListener("message", this._listernerMessage);
+  },
+  unmounted() {
+    window.removeEventListener("message", this._listernerMessage);
+  },
+};
+</script>
